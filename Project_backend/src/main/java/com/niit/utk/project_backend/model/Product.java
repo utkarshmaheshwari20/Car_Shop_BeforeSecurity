7b25215a4bb70fd5a7edbc6258f7f986de046020
utkarshmaheshwari20 <utkarshmaheshwari20@gmail.com>
2018-04-10 15:07:44 +0530

    Add files via upload

---
+++ Project_backend/src/main/java/com/niit/utk/project_backend/model/Product.java
@@ -0,0 +1,96 @@
+package com.niit.utk.project_backend.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+@Table
+
+public class Product {
+	
+	@Id @GeneratedValue
+	private int productId;
+	private String productName;
+	private String productBrandName;
+	private String productDescription;
+	private Integer productPrice;
+	private String imgNames;
+	
+	@Transient
+	private List<MultipartFile> images;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="categoryId")
+	private Category category;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="supplierId")
+	private Supplier supplier;
+	
+	
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductBrandName() {
+		return productBrandName;
+	}
+	public void setProductBrandName(String productBrandName) {
+		this.productBrandName = productBrandName;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public Integer getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(Integer productPrice) {
+		this.productPrice = productPrice;
+	}
+	public Supplier getSupplier() {
+		return supplier;
+	}
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+	public List<MultipartFile> getImages() {
+		return images;
+	}
+	public void setImages(List<MultipartFile> images) {
+		this.images = images;
+	}
+	public String getImgNames() {
+		return imgNames;
+	}
+	public void setImgNames(String imgNames) {
+		this.imgNames = imgNames;
+	}
+	
+}
---
+++ Project_backend/src/main/java/com/niit/utk/project_backend/model/User.java
@@ -0,0 +1,96 @@
+package com.niit.utk.project_backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class User {
+
+	@Id
+	@GeneratedValue
+	private int userId;
+	private String name;
+	private Integer age;
+	private Gender gender;
+	private String address;
+	private String emailId;
+	private String password;
+	private Role userRole;
+
+	public enum Gender {
+		MALE, FEMALE
+	}
+	
+	public enum Role {
+		customer,admin
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(Role userRole) {
+		this.userRole = userRole;
+	}
+
+
+}
